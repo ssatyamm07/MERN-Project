5f9967840565765f3afb7db6db2f2b903729c40e
ssatyamm07 <satyamraj151.rajgir@gmail.com>
2024-04-01 15:01:50 +0530

    Project completed

---
+++ src/component/FilterProduct.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { CiForkAndKnife } from "react-icons/ci";
+
+const FilterProduct = ({ category, onClick, isActive }) => {
+  return (
+    <div onClick={onClick}>
+      <div
+        className={`text-3xl p-5  rounded-full cursor-pointer ${
+          isActive ? "bg-yellow-400" : "bg-red-700"
+        }`}
+      >
+        <CiForkAndKnife />
+      </div>
+      <p className="text-center font-medium my-1 capitalize">{category}</p>
+    </div>
+  );
+};
+
+export default FilterProduct;
---
+++ src/page/Success.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Success = () => {
+  return (
+    <div className="bg-green-200 w-full max-w-sm  m-auto h-36 flex justify-center items-center font-semibold text-lg">
+      <p> Payment is Sucessfully</p>
+    </div>
+  );
+};
+
+export default Success;
